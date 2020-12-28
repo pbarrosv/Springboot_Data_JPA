4fb6d32487262a9551e23ee42443036dde6161a3
luis576 <pedroluisbarrosvalle@hotmail.com>
2020-12-28 01:03:25 -0500

    Creando controlador con la acción handler listar

---
+++ src/main/java/com/springboot/datajpa/controller/ClienteController.java
@@ -0,0 +1,27 @@
+package com.springboot.datajpa.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.springboot.datajpa.dao.IClienteDao;
+import com.springboot.datajpa.model.Cliente;
+
+@Controller
+public class ClienteController {
+	
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@RequestMapping(value = "/listar", method = RequestMethod.GET)
+	public String listarClientes(Model model) {
+		model.addAttribute("titulo","Listado de Clientes");
+		model.addAttribute("clientes",clienteDao.findAllClie());
+		return "listar";
+	}
+}
