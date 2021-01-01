ba58af590c1a267f4180798bcd7a5b49ec66b81d
luis576 <pedroluisbarrosvalle@hotmail.com>
2020-12-31 20:44:29 -0500

    Paginando con PagingAndSortingRepository

--- src/main/java/com/springboot/datajpa/service/ClienteServiceImpl.java
+++ src/main/java/com/springboot/datajpa/service/ClienteServiceImpl.java
@@ -3,6 +3,8 @@ package com.springboot.datajpa.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -40,6 +42,12 @@ public class ClienteServiceImpl implements IClienteService{
 	public void delete(Long id) {
 		clienterepo.deleteById(id);
 		
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Cliente> findAll(Pageable pageable) {
+		return clienterepo.findAll(pageable);
 	}
 
 }
--- src/main/java/com/springboot/datajpa/service/IClienteService.java
+++ src/main/java/com/springboot/datajpa/service/IClienteService.java
@@ -2,10 +2,14 @@ package com.springboot.datajpa.service;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.springboot.datajpa.model.Cliente;
 
 public interface IClienteService {
 	public List<Cliente> findAllClie();
+	public Page<Cliente> findAll(Pageable pageable);
 	public void save(Cliente cliente);
 	public Cliente findOne(Long id);
 	public void delete(Long id);
