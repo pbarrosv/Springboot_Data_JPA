d44d0d6db3ead83121107aec2b7e21aaf33aef81
luis576 <pedroluisbarrosvalle@hotmail.com>
2021-01-02 16:41:44 -0500

    Creando el metodo ver

--- src/main/java/com/springboot/datajpa/controller/ClienteController.java
+++ src/main/java/com/springboot/datajpa/controller/ClienteController.java
@@ -1,6 +1,5 @@
 package com.springboot.datajpa.controller;
 
-
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,91 +32,103 @@ import com.springboot.datajpa.service.IClienteService;
 @Controller
 @SessionAttributes("cliente")
 public class ClienteController {
-	
+
 	@Autowired
 	private IClienteService clienteService;
-	
+
+	@GetMapping(value = "/ver/{id}")
+	public String ver(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+
+		Cliente cliente = clienteService.findOne(id);
+
+		if (cliente == null) {
+			flash.addAttribute("error", "El cliente no existe");
+			return "redirect:/listar";
+		}
+		model.put("cliente", cliente);
+		model.put("titulo", "Detalle cliente: " + cliente.getNombre());
+		return "ver";
+	}
+
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listarClientes(@RequestParam(name = "page", defaultValue = "0")int page, Model model) {
-		
+	public String listarClientes(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+
 		Pageable pageRequest = PageRequest.of(page, 5);
-		
+
 		Page<Cliente> clientes = clienteService.findAll(pageRequest);
 
 		PageRender<Cliente> pageRender = new PageRender<>("/listar", clientes);
-		model.addAttribute("titulo","Listado de Clientes");
-		model.addAttribute("clientes",clientes);
-		model.addAttribute("page",pageRender);
+		model.addAttribute("titulo", "Listado de Clientes");
+		model.addAttribute("clientes", clientes);
+		model.addAttribute("page", pageRender);
 		return "listar";
 	}
-	
+
 	@RequestMapping(value = "/form")
-	public String crear(Map<String,Object> model) {
+	public String crear(Map<String, Object> model) {
 		Cliente clie = new Cliente();
-		model.put("cliente",clie);
-		model.put("titulo", "Formulario de Cliente");		
+		model.put("cliente", clie);
+		model.put("titulo", "Formulario de Cliente");
 		return "form";
 	}
-	
-	
-	@RequestMapping(value = "/form/{id}")
-	public String editar(@PathVariable(value="id") Long id, Map<String, Object> model,RedirectAttributes flash) {
-		
-		Cliente cliente = null;
-		
-		if(id>0) {
-			cliente = clienteService.findOne(id);
-			if(cliente ==null){
 
-				flash.addFlashAttribute("error","El Id del cliente no existe en la BD!");
+	@RequestMapping(value = "/form/{id}")
+	public String editar(@PathVariable(value = "id") Long id, Map<String, Object> model, RedirectAttributes flash) {
+
+		Cliente cliente = null;
+
+		if (id > 0) {
+			cliente = clienteService.findOne(id);
+			if (cliente == null) {
+
+				flash.addFlashAttribute("error", "El Id del cliente no existe en la BD!");
 				return "redirect:/listar";
 			}
 		} else {
-			flash.addFlashAttribute("error","El Id del cliente no puede ser cero!");
+			flash.addFlashAttribute("error", "El Id del cliente no puede ser cero!");
 			return "redirect:/listar";
 		}
 		model.put("cliente", cliente);
 		model.put("titulo", "Editar Cliente");
 		return "form";
 	}
-	
-	
+
 	@RequestMapping(value = "/form", method = RequestMethod.POST)
-	public String guardar(@Valid Cliente cliente, BindingResult result,Model model,@RequestParam("file") MultipartFile foto,RedirectAttributes flash, SessionStatus status) {
-		
-		if(result.hasErrors()) {
-			model.addAttribute("titulo","Formulario de Cliente");
+	public String guardar(@Valid Cliente cliente, BindingResult result, Model model,
+			@RequestParam("file") MultipartFile foto, RedirectAttributes flash, SessionStatus status) {
+
+		if (result.hasErrors()) {
+			model.addAttribute("titulo", "Formulario de Cliente");
 			return "form";
 		}
-		if(!foto.isEmpty()) {
+		if (!foto.isEmpty()) {
 			Path directorioRecursos = Paths.get("src//main//resources//static/uploads");
-			String rootPath =directorioRecursos.toFile().getAbsolutePath();
+			String rootPath = directorioRecursos.toFile().getAbsolutePath();
 			try {
 				byte[] bytes = foto.getBytes();
-				Path rutaCompleta = Paths.get(rootPath +"//"+foto.getOriginalFilename());
+				Path rutaCompleta = Paths.get(rootPath + "//" + foto.getOriginalFilename());
 				Files.write(rutaCompleta, bytes);
-				flash.addFlashAttribute("info","Has subido correctamente '"+ foto.getOriginalFilename()+"'");
+				flash.addFlashAttribute("info", "Has subido correctamente '" + foto.getOriginalFilename() + "'");
 				cliente.setFoto(foto.getOriginalFilename());
 			} catch (Exception e) {
 			}
 		}
-		
-		String mensajeFlash = (cliente.getId() != null)? "Cliente editado con exito!" : "Cliente creado con exito!";
-		
+
+		String mensajeFlash = (cliente.getId() != null) ? "Cliente editado con exito!" : "Cliente creado con exito!";
+
 		clienteService.save(cliente);
 		status.setComplete();
-		flash.addFlashAttribute("success",mensajeFlash);
+		flash.addFlashAttribute("success", mensajeFlash);
 		return "redirect:listar";
 	}
-	
-	@RequestMapping(value="/eliminar/{id}")
-	public String eliminar(@PathVariable(value="id") Long id,RedirectAttributes flash) {
-		if(id>0) {
+
+	@RequestMapping(value = "/eliminar/{id}")
+	public String eliminar(@PathVariable(value = "id") Long id, RedirectAttributes flash) {
+		if (id > 0) {
 			clienteService.delete(id);
-			flash.addFlashAttribute("success","Cliente eliminado con exito");
+			flash.addFlashAttribute("success", "Cliente eliminado con exito");
 		}
-		return"redirect:/listar";
+		return "redirect:/listar";
 	}
-	
-	
+
 }
